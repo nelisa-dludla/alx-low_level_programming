7a578dc46792fe8523153c8c943425113cd2e5ce
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-16 06:29:25 -0700

    Added Task 6 program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/* This program prints the size of various types on the pc */
+
+/**
+ * main - Entry point
+ * printf - prints the various sizes
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
