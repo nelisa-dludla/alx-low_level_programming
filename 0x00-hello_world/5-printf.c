5b7cc9b9360a43efeb85df73068be9aa5c6c42c5
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-16 06:04:47 -0700

    Added Task 5 program and updated Task 4 program permissions

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* This program prints out characters */
+
+/**
+ * main - Entry point
+ * printf - prints "with proper grammer, but the outcome is a piece of art,"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcomr is a piece of art,");
+	return (0);
+}
