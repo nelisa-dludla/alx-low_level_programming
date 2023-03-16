a371b51f01932c97e733bf8a422e6723571d5c49
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-16 04:28:31 -0700

    Updated Task 4 program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle"
+2	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
-	return 0;
+	return (0);
 }
