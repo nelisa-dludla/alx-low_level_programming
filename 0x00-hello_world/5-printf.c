0c47919980c15936119bee4ef851054637f2ced3
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-16 06:10:21 -0700

    Updated Task 5 program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,12 +4,12 @@
 
 /**
  * main - Entry point
- * printf - prints "with proper grammer, but the outcome is a piece of art,"
+ * printf - prints "with proper grammar, but the outcome is a piece of art,"
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
