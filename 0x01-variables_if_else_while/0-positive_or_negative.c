9c7e1c9d324be7c43ccc3eac5068be22eefec4a7
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-17 04:20:46 -0700

    Added Task 1 program and README file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+
+/* This program will print a random number and say whether it is - or + */
+
+/**
+ * main - Entry point
+ * if - Specifies the conditions for printing "n"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0)0;
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
+	return (0);
+}
