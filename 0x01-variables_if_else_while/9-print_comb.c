92890ab2f9df0ac89a835ec934d5efd172d928f7
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-17 11:47:06 -0700

    Updated Task 9 program code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -19,8 +19,11 @@ int main(void)
 	for (num = 48; num < 58; num++)
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
