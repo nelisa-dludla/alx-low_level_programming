8446fb134306b3d3a5498785240ec71844936586
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-17 06:07:14 -0700

    Updated Task 1 program code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,16 +15,14 @@
 
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of %d is ", n);
-
-	int last_digit;
-
 	last_digit = n % 10;
+
+	printf("Last digit of %d is ", n);
 
 	if (last_digit > 5)
 	{
