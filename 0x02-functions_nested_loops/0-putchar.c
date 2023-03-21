f628760627829252d28a2c7428a677fc4dbadef4
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-21 02:19:57 -0700

    Added the README file and Task 0 program code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ * This program prints '_putchar'
+ * followed by a new line
+ */
+
+/**
+ * main - Entry point
+ * _putchar - function using putchar to print '_putchar'
+ *@c: The character to be printed
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char c);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,7 @@
+**In this directory, you will find all the C programs and functions I have created for my C Projects:**
+
+1. 0-putchar.c - A program that prints \_putchar, followed by a new line
+
+2. 1-alphabet.c - A function that prints the alphabet, in lowercase, followed by a new line
+
+3. 
