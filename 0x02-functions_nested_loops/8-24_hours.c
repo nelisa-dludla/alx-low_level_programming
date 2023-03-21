e042b67d6df26402c4af552558dc7c3ee3afc0a5
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-21 14:02:57 -0700

    Added Task 8 function code

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/*
+ * This function prints every minute of the day of
+ * Jack Bauer, starting from 00:00 to 23:59
+ */
+
+/**
+ * jack_bauer - Function prints every minute of the day
+ * Return: Void
+ */
+
+void jack_bauer(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 23; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 59; num2++)
+		{
+			_putchar((num1 / 10) + '0');
+			_putchar((num1 % 10) + '0');
+			_putchar(':');
+			_putchar((num2 / 10) + '0');
+			_putchar((num2 % 10) + '0');
+			_putchar('\n');
+
+			if (num1 == 23 && num2 == 59)
+			{
+				break;
+			}
+		}
+	}
+}
