06602e1745624dc5052d212cfe44a5472ef20557
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-22 08:30:01 -0700

    Added Task 10 function code

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * This function adds two integers and returns the result
+ */
+
+/**
+ * add - Function adds the two integers and returns the result
+ * @num1: The value of one integer
+ * @num2: The value of the second integer
+ * Return: Result
+ */
+
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
