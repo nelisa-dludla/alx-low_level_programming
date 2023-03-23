7d3c769619cb46a9d30635a425987d69ec68a6cc
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-23 15:34:30 -0700

    Added Task 9 program code

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/*
+ * This program prints the numbers from 1 - 100, followed by a new line
+ * multiples of 3 will be replaced with 'Fizz', multiples of 5 with 'Buzz
+ * and multiples of both with FizzBuzz
+ */
+
+/**
+ * main - Entry point
+ * num -  will print a number OR Fizz OR Buzz OR FizzBuzz
+ *
+ * for loop - prints the numbers as while as the placed words for
+ * certain numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 5 == 0 && num % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
