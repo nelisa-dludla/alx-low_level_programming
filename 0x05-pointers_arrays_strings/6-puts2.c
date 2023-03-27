85e03a0bba16b9b0b2a2cba49e0776b38d0a86e6
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-27 23:14:18 +0200

    Added Task 6 function code and test file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/*
+ * This function prints every other character of a string,
+ * starting with the first character, followed by a new line
+ */
+
+/**
+ * puts2 - Function prints every other character of a string
+ * @str: Pointer value
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
