66b3b0c65fbf86721f7f058c99554cfe9309e28c
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-31 14:21:30 +0200

    Updated Task 3 code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,10 +19,6 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] && s1[i] == s2[i])
 	{
-		if (s1[i] != '\0')
-		{
-			return (0);
-		}
 		i++;
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
