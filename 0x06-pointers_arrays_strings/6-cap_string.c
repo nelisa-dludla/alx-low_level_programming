4a873d017eef8d045ce7914a1b9f4b8a2d2ad3b9
nelisa-dludla <nelisa.dludla@outlook.com>
2023-03-31 12:00:28 +0200

    Updated Task 6 code to accomodate other use cases

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,19 +22,19 @@ char *cap_string(char *str)
 		}
 	}
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i])
 	{
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
-		|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
-		|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
-		|| str[i - 1] == '(' || str[i - 1] == ')'
-		|| str[i - 1] == '{' || str[i - 1] == '}')
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-			}
+			i++;
 		}
+
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
 	}
+
 	return (str);
 }
