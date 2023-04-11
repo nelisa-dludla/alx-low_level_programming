7c67c9955ab3ecfab870ec38f2a5fbbbecb0c480
nelisa-dludla <nelisa.dludla@outlook.com>
2023-04-12 01:07:38 +0200

    Updated Task 3 function code - Added free function to help with mallocs return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,22 +21,31 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	grid = (int **) malloc(height * sizeof(int *));
-
-	for (i = 0; i < height; i++)
-	{
-		grid[i] = (int *) malloc(width * sizeof(int));
-
-		for (j = 0; j < width; j++)
-		{
-			grid[i][j] = 0;
-		}
-	}
+	grid = (int **)malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
 
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+
+		if (grid == NULL)
+		{
+			for (j = 0; j < width; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
 	return (grid);
 }
