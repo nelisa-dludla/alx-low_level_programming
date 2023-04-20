7f30d7834ada8d552e3eafa22ecac06aec3a2ccc
nelisa-dludla <nelisa.dludla@outlook.com>
2023-04-21 00:19:19 +0200

    Updated/Overhauled Task 3 code - I was failing too many checks

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,53 +7,6 @@
  */
 
 /**
- * print_char - Prints a single character
- * @c: Character to be printed
- */
-
-void print_char(int c)
-{
-	printf("%c", (char)c);
-}
-
-/**
- * print_int - Prints an integer
- * @i: Integer to be printed
- */
-
-void print_int(int i)
-{
-	printf("%i", i);
-}
-
-/**
- * print_float - Prints a float
- * @f: FLoat to be printed
- */
-
-void print_float(double f)
-{
-	printf("%f", f);
-}
-
-/**
- * print_string - Prints a string
- * @s: String to be printed
- */
-
-void print_string(char *s)
-{
-	if (s == NULL)
-	{
-		printf("(nil)");
-	}
-	else
-	{
-		printf("%s", s);
-	}
-}
-
-/**
  * print_all - Function prints anything
  * @format: type of input to be printed
  */
@@ -61,40 +14,42 @@ void print_string(char *s)
 void print_all(const char *const format, ...)
 {
 	va_list args;
-	int i = 0;
-	char *separator = "";
+	char c, *s;
+	int i, pos;
+	float f;
 
 	va_start(args, format);
-
-	while (format[i] != '\0')
+	while (format[pos] != '\0')
 	{
-		switch (format[i])
+		switch (format[pos])
 		{
 		case 'c':
-			printf("%s", separator);
-			print_char(va_arg(args, int));
+			c = (char) va_arg(args, int);
+			printf("%c", c);
 			break;
 
 		case 'i':
-			printf("%s", separator);
-			print_int(va_arg(args, int));
+			i = va_arg(args, int);
+			printf("%d", i);
 			break;
 
 		case 'f':
-			printf("%s", separator);
-			print_float(va_arg(args, double));
+			f = va_arg(args, double);
+			printf("%f", f);
 			break;
 
 		case 's':
-			printf("%s", separator);
-			print_string(va_arg(args, char *));
+			s = va_arg(args, char *);
+			printf("%s", s);
 			break;
 
 		default:
-			break;
+			pos++;
+			continue;
 		}
-		separator = ", ";
-		i++;
+		pos++;
+		if (format[pos] != '\0')
+			printf(", ");
 	}
 	va_end(args);
 	printf("\n");
