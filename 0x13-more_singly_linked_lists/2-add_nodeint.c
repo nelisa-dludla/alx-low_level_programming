26ed132e26c8e2a9f06a34ea7908116fa188dd32
nelisa-dludla <nelisa.dludla@outlook.com>
2023-05-04 00:57:08 +0200

    Updated Task 2 function code - Added a check for newNode after using malloc

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,7 +18,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	}
 
 	newNode = (listint_t *) malloc(sizeof(listint_t));
-
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
 
 	newNode->n = n;
 	newNode->next = *head;
