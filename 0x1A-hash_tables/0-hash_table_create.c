cdf672f47d18c198b1780baee761c76fb11d4bbd
nelisa-dludla <nelisa.dludla@outlook.com>
2023-11-04 04:17:56 +0200

    Reverted changes for task 0 back to original commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,25 +7,16 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *result;
-	unsigned long int i;
+	hash_table_t *result = malloc(sizeof(hash_table_t));
 
-	result = malloc(sizeof(hash_table_t));
 	if (result == NULL)
 		return (NULL);
 
 	result->size = size;
 	result->array = malloc(sizeof(hash_node_t) * size);
-	if (result->array == NULL)
-	{
-		free(result);
-		return (NULL);
-	}
 
-	for (i = 0; i < size; i++)
-	{
-		result->array[i] = NULL;
-	}
+	if (result->array == NULL)
+		return (NULL);
 
 	return (result);
 }
